d8b5d6bcecab9e3193cb82e9fe5dbe860d69945f
Hafidh Rizky <hafidhrzk@gmail.com>
2023-07-18 00:38:40 +0700

    descending

---
+++ descending_order.js
@@ -0,0 +1,17 @@
+
+const url = "https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript"
+
+function descendingOrder(n) {
+    return Number(String(n).split('').sort().reverse().join(''));
+}
+
+const input1 = 42145;
+const expected1 = 54421;
+
+const input2 = 145263
+const expected2 = 654321
+
+const input3 = 123456789 
+const expected13 = 987654321
+
+console.log(descendingOrder(input1))
